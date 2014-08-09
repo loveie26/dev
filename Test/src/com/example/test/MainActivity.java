a089698d2c8033fa46bdda40bfc919ed9383fdfc
wangliangliang <wangliangliang@tuniu.com>
2014-08-09 21:48:40 +0800

    Revert "test github"
    This reverts commit 6cd1fbb03e0853f5023cf4297609702c591c8b04.

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -12,8 +12,4 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
-    
-    private void teskGithub(){
-    }
 }
